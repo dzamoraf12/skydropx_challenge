3e65934cb09be16cb2c429a4bf93061de4a6cc20
Daniel Zamora <ernesto.zamora@gcabsa.com.mx>
2020-10-07 15:52:21 -0700

    Se agrega lógica para cachar los exceptions
    - Se agregan algunos mensajes de error en el archivo en.yml
    - Se agregan rescues generales para el controlados de shipments, para detectar las paqueterias no implementadas, error al crear shipments sin datos y errores inesperados.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+
+  def render_json_error(status, error_code, extra = {})
+    status = Rack::Utils::SYMBOL_TO_STATUS_CODE[status] if status.is_a? Symbol
+
+    error = {
+      title: I18n.t("error_messages.#{error_code}.title"),
+      status: status
+    }.merge(extra)
+
+    detail = I18n.t("error_messages.#{error_code}.detail", default: '')
+    error[:detail] = detail unless detail.empty?
+
+    render json: { error: error }, status: status
+  end
 end
